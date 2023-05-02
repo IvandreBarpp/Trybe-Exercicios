86445f345e1d5b95cd437af55befb0bd5efbba63
IvandreBarpp <ivandrebarpp@gmail.com>
2023-05-02 16:28:51 -0300

    primeiros exercícios javascript

--- Fundamentos/secao-3-introducao-javascript/exercicio.js
+++ Fundamentos/secao-3-introducao-javascript/exercicio.js
@@ -1,5 +1,5 @@
-let name = 'ivandre'
-let birthcity = 'canoas'
-let birthday = '24/02/1999'
+let name = 'ivandre';
+let birthcity = 'canoas';
+let birthday = '24/02/1999';
 
 console.log('Meu nome é ' + name,', nasci na cidade de ' + birthcity,', na data ' + birthday);
