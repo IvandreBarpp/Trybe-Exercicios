def9a677980a96a21f6ac5ad819b644106bf8f1c
IvandreBarpp <ivandrebarpp@gmail.com>
2023-05-08 18:27:09 -0300

    exercicios atualizados

--- Fundamentos/secao-3-introducao-javascript/dia-2-array-e-for/exercicio4.js
+++ Fundamentos/secao-3-introducao-javascript/dia-2-array-e-for/exercicio4.js
@@ -48,14 +48,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // EX6:
-// let menorNumero = numbers[0];
+let menorNumero = numbers[0];
 
-// for (let index = 0; index < numbers.length; index += 1) {
-//   if (numbers[index] < menorNumero) {
-//     menorNumero = numbers[index];
-//   }
-// }
-// console.log(menorNumero);
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > menorNumero) {
+    menorNumero = numbers[index];
+  }
+}
+console.log(menorNumero);
 
 // EX7:
 // let numbers1 = [];
