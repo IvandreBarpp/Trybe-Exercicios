513ba47d858074bbbcf478b8ca0b2cd4535250a7
IvandreBarpp <ivandrebarpp@gmail.com>
2023-06-12 22:04:59 -0300

    organizando

--- Fundamentos/secao-3-introducao-javascript/dia-1-primeiros-passos/exercicio.js
+++
@@ -1,5 +0,0 @@
-let userName = 'ivandre';
-let birthcity = 'canoas';
-let birthday = '24/02/1999';
-
-console.log('Meu nome é ' + userName,', nasci na cidade de ' + birthcity,', na data ' + birthday);
--- Fundamentos/secao-3-introducao-javascript/dia-1-primeiros-passos/exercicio7.js
+++
@@ -1,8 +0,0 @@
-let a = 1;
-let b = 2;
-
-if (a > b) {
-  console.log('O número maior é ' + a);
-} else {
-  console.log('O número maior é ' + b);
-}
