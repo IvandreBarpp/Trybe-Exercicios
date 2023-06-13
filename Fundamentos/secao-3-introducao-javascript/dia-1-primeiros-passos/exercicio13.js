6303fdcb36eef6156f4bdb76d4e4631c1ee1548a
IvandreBarpp <ivandrebarpp@gmail.com>
2023-06-12 22:15:10 -0300

    organizando

--- Fundamentos/secao-3-introducao-javascript/dia-1-primeiros-passos/exercicio13.js
+++
@@ -1,9 +0,0 @@
-let a = 3;
-let b = 5;
-let c = 10;
-
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
-  console.log(true);
-} else {
-  console.log(false);
-}
--- Fundamentos/secao-3-introducao-javascript/dia-2-array-e-for/exercicio.js
+++
@@ -1,8 +0,0 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[menu.length -3];
-let indexOfPortfolio = menu.indexOf('Portfólio');
-menu.push('Contato');
-
-console.log(menuServices);
-console.log(indexOfPortfolio);
-console.log(menu);
