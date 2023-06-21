bdb4bce0c2c0962fdcfd1865a5da0a55b2aea8d8
IvandreBarpp <ivandrebarpp@gmail.com>
2023-06-21 13:13:52 -0300

    exercicios dia 5

---
+++ 1-Fundamentos/secao-7-higher-order-functions-javascriptES6/dia-5-array-default-obj-destructuring/array-destructuring.js
@@ -0,0 +1,30 @@
+const primeNumbers = [17, 23, 37];
+const [firstNumber, secondNumber, thirdNumber] = primeNumbers;
+
+const sum = (a, b) => a + b;
+
+// console.log(sum(firstNumber, thirdNumber)); // 54
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+// ------------------------------------------------------------------------
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+// ------------------------------------------------------------------------
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
